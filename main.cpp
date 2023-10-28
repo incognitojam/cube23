dc025aea3abbea83d9ac419f29e04ae7ee8027c1
Cameron Clough <cameronjclough@gmail.com>
2023-10-28 23:46:40 +0100

    add more app info

--- main.cpp
+++ main.cpp
@@ -319,6 +319,10 @@ private:
         {
             appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
             appInfo.pApplicationName = "Hello Triangle";
+            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.pEngineName = "No Engine";
+            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.apiVersion = VK_API_VERSION_1_0;
         }
 
         VkInstanceCreateInfo createInfo{};
