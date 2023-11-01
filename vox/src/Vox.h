7dbd3c44068f12f93846d291d1b1241728b0a272
Cameron Clough <cameronjclough@gmail.com>
2023-11-01 12:19:30 +0000

    add key and mouse codes

--- vox/src/Vox.h
+++ vox/src/Vox.h
@@ -2,6 +2,10 @@
 
 #include "vox/application.h"
 
+#include "vox/input.h"
+#include "vox/key_codes.h"
+#include "vox/mouse_button_codes.h"
+
 // ---Entry Point ---------------
 #include "vox/entry_point.h"
 // ------------------------------
--- vox/src/vox/application.cpp
+++ vox/src/vox/application.cpp
@@ -21,14 +21,6 @@ namespace Vox {
     }
 
     void Application::run() {
-        WindowResizeEvent e(1280, 720);
-        if (e.isInCategory(EventCategoryApplication)) {
-            std::cout << e << std::endl;
-        }
-        if (e.isInCategory(EventCategoryInput)) {
-            std::cout << e << std::endl;
-        }
-
         while (mRunning) {
             glClearColor(0.5f, 0.0f, 0.5f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
